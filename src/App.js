2719bf12a261a4290b32e868002fa204e4569e27
Joseph Panetta <jlpanetta1681@icloud.com>
2022-05-17 06:15:13 -0400

    making the component structure for band/stage name generator, connecting the api now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './Styles/App.css';
-
+import { React, useState } from 'react';
 function App() {
-  return (
+  return () => {
+  
+     const [genre, setGenre] = useState('');
+     const [name, setName] = useState('');
+     const [instrument, setInstrunent] = useState('');
     <div className="App">
-      <h1>Choose a Genre! Get a cool band name!</h1>
-      <h2>Give me some information and I will generate cool stage names</h2>
-      <h2>for you and your band-mates</h2>
+      <div className="directions">
+        <h1>Choose a genre! Get a cool band name!</h1>
+        <h2>Give me some information and I will generate cool stage names</h2>
+        <h2>for you and your band-mates</h2>
+
+    </div>
+      <div className="inputFields">
+        <input type="text" placeholder="Enter a genre" />
+        <input type="text" placeholder="Enter your name" />
+        <input type="text" placeholder="What instrument do you play?" />
       </div>
-  );
-}
+    </div>
+      );
+
 
 export default App;
