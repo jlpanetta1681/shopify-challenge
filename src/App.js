3b7344091052e67399fccc8b6de9b873da3a39df
Joseph Panetta <jlpanetta1681@icloud.com>
2022-05-17 06:59:56 -0400

    got yhe connectrio to the openai api started, donr for the day.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './Styles/App.css';
 import { React, useState } from 'react';
+
 function App() {
-  return () => {
+
+  const [genre, setGenre] = useState("");
+  const [name, setName] = useState("");
+  const [instrument, setInstrument] = useState("");
+  const [message, setMessage] = useState("");
   
-     const [genre, setGenre] = useState('');
-     const [name, setName] = useState('');
-     const [instrument, setInstrunent] = useState('');
+  
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("api.openai.com/v1/engines/text-davinci-002/completions", {
+        method: "GET",
+        body: JSON.stringify({
+          genre: genre,
+          name: name,
+          instrument: instrument,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setGenre("");
+        setName("");
+        setInstrument("");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  
+  
+  return (
+
     <div className="App">
       <div className="directions">
         <h1>Choose a genre! Get a cool band name!</h1>
@@ -13,13 +44,35 @@ function App() {
         <h2>for you and your band-mates</h2>
 
     </div>
-      <div className="inputFields">
-        <input type="text" placeholder="Enter a genre" />
-        <input type="text" placeholder="Enter your name" />
-        <input type="text" placeholder="What instrument do you play?" />
-      </div>
-    </div>
-      );
+    <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={genre}
+          placeholder="enter genre"
+          onChange={(e) => setGenre(e.target.value)}
+        />
+        <input
+          type="text"
+          value={name}
+          placeholder="what is your name?"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={instrument}
+          placeholder="instrument(optional for stage name)"
+          onChange={(e) => setInstrument(e.target.value)}
+        />
+         <button type="submit">Create</button>
 
+           <div className="message">
+               {message ? 
+               <p>
+                   {message}
+               </p> : null}
+            </div>
+    </form>
+</div>
+  )}
 
 export default App;
