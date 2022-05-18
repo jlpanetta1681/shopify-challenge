7b235e8184f58dc4f7decb12c154056bd90967fa
Joseph Panetta <jlpanetta1681@icloud.com>
2022-05-18 17:15:26 -0400

    finishing fixing the api key not being sent

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import './Styles/App.css';
 import { React, useState } from 'react';
 
+let APIKey = process.env.OPENAI_API_KEY;
+
+
 function App() {
+  console.log(APIKey)
 
   const [genre, setGenre] = useState("");
   const [name, setName] = useState("");
-  const [instrument, setInstrument] = useState("");
   const [message, setMessage] = useState("");
+  
   
   
   let handleSubmit = async (e) => {
     e.preventDefault();
+    const request = `Create a band name using ${genre} ${name}`
+    console.log(request)
     try {
-      let res = await fetch("api.openai.com/v1/engines/text-davinci-002/completions", {
-        method: "GET",
+      let res = fetch("https://api.openai.com/v1/engines/davinci/completions", {
+        method: "POST",
+       
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${APIKey}`
+        }),
         body: JSON.stringify({
           genre: genre,
           name: name,
-          instrument: instrument,
-        }),
-      });
-      let resJson = await res.json();
+          
+  "prompt": "Say this is a test",
+  "max_tokens": 5,
+  "temperature": 1,
+  "top_p": 1,
+  "n": 1,
+  "stream": false,
+  "logprobs": null,
+  "stop": "\n"
+}
+      
+        ),
+    });
+      // let resJson = await res.json();
       if (res.status === 200) {
         setGenre("");
         setName("");
-        setInstrument("");
+  
       } else {
         setMessage("Some error occured");
       }
@@ -37,11 +58,13 @@ function App() {
   
   return (
 
-    <div className="App">
+    <div className="App" >
+   
+      
       <div className="directions">
         <h1>Choose a genre! Get a cool band name!</h1>
-        <h2>Give me some information and I will generate cool stage names</h2>
-        <h2>for you and your band-mates</h2>
+        <h2>Give me some information and I will generate a name for your band</h2>
+    
 
     </div>
     <form onSubmit={handleSubmit}>
@@ -57,12 +80,7 @@ function App() {
           placeholder="what is your name?"
           onChange={(e) => setName(e.target.value)}
         />
-        <input
-          type="text"
-          value={instrument}
-          placeholder="instrument(optional for stage name)"
-          onChange={(e) => setInstrument(e.target.value)}
-        />
+       
          <button type="submit">Create</button>
 
            <div className="message">
