ea3bb63c820b8acf46a5fb235b4df16625720ab6
Joseph Panetta <jlpanetta1681@icloud.com>
2022-05-20 21:41:15 -0400

    got the reactDOM error fixed but still wont retuirn anything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import BandNameGenerator from '../src/components/BandNameGenerator.js';
 
 function App() {
@@ -15,5 +14,7 @@ return (
 );
 
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import { ReactDOM } from 'react'
-import App from '../src/App.js';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+import App from './App';
+
+// 👇️ IMPORTANT: use correct ID of your root element
+// this is the ID of the div in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
